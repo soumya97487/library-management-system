3eb788507fce0b0b89a821426dadc384c96a77dc
Soumyajit Shome <soumyajitshome4@gmail.com>
2025-05-22 22:51:48 +0530

    Adding Schemas

--- models/Loans.js
+++ models/Loans.js
@@ -1,5 +1,12 @@
 const mongoose = required('mongoose')
 
 const loanSchema = new mongoose.schema({
-    
+    book: {type: mongoose.schema.Types.ObjectId, ref: 'Book', required: true},
+    borrower: {type: mongoose.schema.Types.ObjectId, ref: 'Borrower', required: true},
+    loan_date: {type: Date, default: Date.now},
+    due_date: {type: Date, required: true},
+    return_date: {type: date, required: true},
+    status: {type: String, default: 'on_loan'}
 })
+
+module.exports = mongoose.model('Loan', loanSchema);
