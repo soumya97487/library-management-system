b8f2689db1bc82f616b0b1d90e0180b6da44d578
Soumyajit Shome <soumyajitshome4@gmail.com>
2025-05-23 23:20:28 +0530

    initiate changes

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const connectDB = require('./config/db')
+require('dotenv').config()
 
-const PORT = process.env.PORT || 3000;
+const app = express()
 
-app.use((req, res, next) => {
-  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-  console.log(`\n API Request Received`);
-  console.log(` Full URL: ${fullUrl}`);
-  console.log(` Base Path: ${req.protocol}://${req.get('host')}`);
-  console.log(`Endpoint: ${req.originalUrl}`);
-  console.log(`HTTP Method: ${req.method}`);
-  next();
-});
+app.use(express.json())
 
+connectDB()
 
-app.get('/api/info', (req, res) => {
-  res.send('API called. Check the console for details.');
-});
+app.use('/api/authors', require('./routes/authors'))
+app.use('/api/books', require('./routes/books'))
+app.use('/api/categories', require('./routes/categories'))
+app.use('/api/borrowers', require('./routes/borrowers'))
+app.use('/api/loans', require('./routes/loans'))
 
+const PORT = process.env.PORT
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/api/info`);
-});
+app.listen(PORT, ()=>console.log(`server is runninmg on port ${PORT}`))
